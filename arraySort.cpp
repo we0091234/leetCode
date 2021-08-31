d38f06fb1509346faccab37335082011749eb637
we0091234 <447587096@qq.com>
2021-08-31 21:50:06 +0800

    插入排序

--- arraySort.cpp
+++ arraySort.cpp
@@ -8,10 +8,11 @@ void show(vector<int> p)
     cout<<endl;
 }
 
-int  quickSort(vector<int> & p,int i,int j )
+void  quickSort(vector<int> & p,int i,int j )
 {
-    if(i>=j)                     //递归出口
-    return  0;
+    
+    if(i<j)
+    {
     int temp =p[i];
     int left =i,right =j;
     while(left<right)
@@ -32,9 +33,10 @@ int  quickSort(vector<int> & p,int i,int j )
     p[left]=temp;
     quickSort(p,i,left-1);
     quickSort(p,left+1,j);
+    }
     
 }
-void insertSort(vector<int> &array)  //插入排序
+void insertSort(vector<int> &array)  //插入排序我的实现1
 {
     int i = 1;
     int index= 1;  //当前待插入的数组长度
@@ -52,12 +54,49 @@ void insertSort(vector<int> &array)  //插入排序
     }
 }
 
+void insertSort2(vector<int> &array)  //插入排序 参考他人实现
+{
+   int pos ,tmp;
+   for(int i = 1; i<array.size(); i++)
+   {
+       tmp = array[i];
+       pos=i-1;
+       while(pos>=0 && array[pos]>tmp)
+       {
+           array[pos+1]=array[pos];
+           pos--;
+       }
+       array[pos+1]=tmp;
+   }
+
+}
+void insertSort3(vector<int>&array) //插入排序
+{
+    for(int i = 1; i<array.size(); i++)
+    {
+        int  tmp = array[i];
+        int j;
+        for(j = i-1;j>=0 &&array[j]>tmp;j--)
+        {
+              array[j+1]=array[j];
+        }
+         array[j+1]=tmp;
+    }
+}
+
+
+void MergeArray(vector<int>&array)
+{
+    
+}
+
+
 int main(int argc, char **argv)
 {
-    vector<int> array{1,3,2,5,7,5,4};
+    vector<int> array{9,8,7,6,5,4,3,2,10};
     show(array);
-    // quickSort(array,0,4);
-    insertSort(array);
+    quickSort(array,0,array.size()-1);
+    // insertSort3(array);
      show(array);
     return 0;
 }
