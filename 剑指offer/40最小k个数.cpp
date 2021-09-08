0f8febe284a8e327e4e6dea75f8f39389841ca32
we0091234 <447587096@qq.com>
2021-09-08 08:09:03 +0800

    heap

--- 剑指offer/40最小k个数.cpp
+++ 剑指offer/40最小k个数.cpp
@@ -10,7 +10,7 @@ void show(vector<int> p)
     cout<<s<<" ";
     cout<<endl;
 }
-void quickSort(vector<int> &array,int left,int right)
+void quickSort(vector<int> &array,int left,int right)  //使用快速排序
 {
     if(left<right)
     {
@@ -86,6 +86,40 @@ int  quickSort1(vector<int> &array,int left,int right,int k)
          return vector<int> (arr.begin(),arr.begin()+k);
     }
 
+
+//方法2 构建最大堆
+void heapfy(vector<int> &nums,int len,int parent) //将parent变为最大的
+{
+ int leftChild = 2*parent+1;
+ while(leftChild<len)
+ {
+     if(leftChild+1<len && nums[leftChild]<nums[leftChild+1])
+     {
+         leftChild++;
+     }
+     if(nums[parent]>nums[leftChild])
+     return ;
+     swap(nums[parent],nums[leftChild]);
+     parent =leftChild;
+     leftChild=2*leftChild+1;
+ }
+}
+void buildHeap(vector<int>&nums)
+{
+    for(int i = (nums.size()-2)/2;i>=0; i--)
+    heapfy(nums,nums.size(),i);
+}
+
+void heapSort(vector<int> &nums)
+{
+    buildHeap(nums);
+    for(int i = nums.size()-1; i>0; i--)
+    {
+        swap(nums[0],nums[i]);
+        heapfy(nums,i,0);
+    }
+}
+
     
 int main(int argc, char **argv)
 {
@@ -94,7 +128,9 @@ int main(int argc, char **argv)
     // quickSort(MerGearray1,0,MerGearray1.size()-1);
     // show(MerGearray1);
     // show(MerGearray1);
-    show(getLeastNumbers1(array,4));
+    // show(getLeastNumbers1(array,4));
+    heapSort(MerGearray1);
+    show(MerGearray1);
     return 0;
 }
 
