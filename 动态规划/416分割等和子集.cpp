a6a632c5d8cce42501b2e93005f46e8e4b909f8b
we0091234 <447587096@qq.com>
2021-11-18 18:01:48 +0800

    dp

--- 动态规划/416分割等和子集.cpp
+++ 动态规划/416分割等和子集.cpp
@@ -4,6 +4,8 @@
 // 示例 1: 输入: [1, 5, 11, 5] 输出: true 解释: 数组可以分割成 [1, 5, 5] 和 [11].
 // 示例 2: 输入: [1, 2, 3, 5] 输出: false 解释: 数组不能分割成两个元素和相等的子集.
 
+//1确定dp[j] 表示背包容量为j的最大子和为dp[j]
+
 
 #include <iostream>
 #include <vector>
@@ -14,11 +16,24 @@ bool canPartition(std::vector<int>& nums)
     sum+=s;
     if(sum%2!=0)
     return false;
+    int val = sum/2;
+    std::vector<int> dp(100001,0);
+    for(int i = 0; i<nums.size(); i++) //遍历物品
+    {
+        for(int j = val;j>=nums[i];j--)
+        {
+            dp[j]=std::max(dp[j],dp[j-nums[i]]+nums[i]);
+        }
     
+    }
+    if(dp[val]==val)
+        return true;
+   return false;
 
 }
 int main(int argc, char **argv)
 {
-    std::vector<int> nums{1,5,11,5};
+    std::vector<int> nums{1,5,11,5,1};
+    std::cout<<canPartition(nums)<<std::endl;
     return 0;
 }
